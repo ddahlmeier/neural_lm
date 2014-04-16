41d479cb0f96be01f51bbe436db7d352d4b15f97
Daniel Dahlmeier <d.dahlmeier@sap.com>
2014-04-16 13:41:53 +0800

    added different options for learning rate.

--- lbl.py
+++ lbl.py
@@ -1,31 +1,42 @@
 """
 
-The lob-bilinear language model from (Mnih and Teh, ICML 2012)
+The log-bilinear language model from (Mnih and Teh, ICML 2012)
 
 References:
- A fast and simple algorithm for training neural probabilistic language models. Andriy Mnih and Yee Whye Teh.
+ A fast and simple algorithm for training neural probabilistic language models. 
+ Andriy Mnih and Yee Whye Teh.
  International Conference on Machine Learning 2012 (ICML 2012) 
 
-Usage: lbl.py [-v] [-k WORD_DIM] [-n CONTEXT_SZ] [-l LEARN_RATE] [-e EPOCHS] [-b BATCH_SZ] [-s SEED] TRAIN_DATA [DEV_DATA] [TEST_DATA]
+Usage: lbl.py [--verbose] [--word_dim WORD_DIM] [--context_sz CONTEXT_SZ] 
+              [--learn_rate LEARN_RATE] [--rate_update RATE_UPDATE] 
+              [--epochs EPOCHS] [--batch_size BATCH_SZ] [--seed SEED]  
+              [--patience PATIENCE] [--patience_incr PATIENCE_INCR] 
+              [--improvement_thrs IMPR_THRS] [--validation_freq VALID_FREQ] 
+              <train_data> <dev_data> [<test_data>]
 
 Arguments:
- TRAIN_DATA       training data of tokenized text, one sentence per line.
- DEV_DATA         development data of tokenized text, one sentence per line.
- TEST_DATA        test data of tokenized text, one sentence per line.
+ train_data       training data of tokenized text, one sentence per line.
+ dev_data         development data of tokenized text, one sentence per line.
+ test_data        test data of tokenized text, one sentence per line.
 
 Options:
-    -v, --verbose                            Print debug information
-    -k WORD_DIM, --word_dim=WORD_DIM         dimension of word embeddings, default 20.
-    -n CONTEXT_SZ, --context_sz=CONTEXT_SZ   size of n-gram context window, default 2.
-    -l LEARN_RATE, --learn_rate=LEARN_RATE   learning rate parameter, default 0.1.
-    -e EPOCHS, --epochs=EPOCHS               number of training epochs, default 10.
-    -b BATCH_SIZE, --batch_size=BATCH_SIZE   size of mini-batch for training, default 100.
-    -s SEED, --seed=SEED                     seed for random generator.
-
+    -v, --verbose                                Print debug information
+    -k WORD_DIM, --word_dim=WORD_DIM             dimension of word embeddings [default: 100]
+    -n CONTEXT_SZ, --context_sz=CONTEXT_SZ       size of n-gram context window [default: 2]
+    -l LEARN_RATE, --learn_rate=LEARN_RATE       initial learning rate parameter [default: 1]
+    -u RATE_UPDATE, --rate_update=RATE_UPDATE    learning rate update: 'simple', 'adaptive' [default: simple]
+    -e EPOCHS, --epochs=EPOCHS                   number of training epochs [default: 10]
+    -b BATCH_SIZE, --batch_size=BATCH_SIZE       size of mini-batch for training [default: 100]
+    -s SEED, --seed=SEED                         seed for random generator.
+    -p PATIENCE, --patience PATIENCE             min number of examples to look before stopping, default is no early stopping
+    -i PATIENCE_INCR, --patience_incr=PATIENCE   wait for this much longer when a new best result is found [default: 2]
+    -t IMPR_THRS, --improvement_thrs=IMPR_THRS   a relative improvemnt of this is considered significant [default: 0.995]
+    -f VALID_FREQ, --validation_freq=VALID_FREQ  number of examples after which check score on dev set [default: 1000]
 """
 
 from dataset import Dictionary, load_corpus
 
+import cPickle
 import numpy as np
 import theano
 import theano.tensor as T
@@ -33,9 +44,6 @@ import math
 from future_builtins import zip
 import time
 import logging
-
-
-# Usage: lbl.py [-v] [-k WORD_DIM] [-n CONTEXT_SZ] [-l LEARN_RATE] [-e EPOCHS] [-b BATCH_SZ] [-s SEED] TRAIN_DATA [DEV_DATA] [TEST_DATA]
 
 logger = logging.getLogger(__name__)
 
@@ -49,11 +57,11 @@ class LogBilinearLanguageModel(object):
         """
         Initialize the parameters of the language model
         """
-
         # training contexts
         self.context = context
 
         # initialize context word embedding matrix R of shape (V, K)
+        # TODO: parameterize initialization
         R_values = np.asarray(rng.uniform(-0.01, 0.01, size=(V, K)), dtype=theano.config.floatX)
         self.R = theano.shared(value=R_values, name='R', borrow=True)
 
@@ -78,18 +86,32 @@ class LogBilinearLanguageModel(object):
         # softmax activation function
         self.p_w_given_h = T.nnet.softmax(self.s)
 
+        # compute prediction as class whose probability is maximal in
+        # symbolic form
+        self.y_pred = T.argmax(self.p_w_given_h, axis=1)
+
         # parameters of the model
         self.params = [self.R, self.Q, self.C, self.b]
 
-
     def negative_log_likelihood(self, y):
-        return -T.mean(T.log(T.transpose(self.p_w_given_h))[T.arange(y.shape[0]),y])
+        # take the logarithm with base 2
+        return -T.mean(T.log2(T.transpose(self.p_w_given_h))[T.arange(y.shape[0]),y])
 
-    def perplexity(self, y):
-        return T.pow(T.prod(T.inv(T.transpose(self.p_w_given_h)[T.arange(y.shape[0]),y])), 1.0/y.shape[0])
 
+    def errors(self, y):
+        # check if y has same dimension of y_pred
+        if y.ndim != self.y_pred.ndim:
+            raise TypeError('y should have the same shape as self.y_pred',
+                ('y', target.type, 'y_pred', self.y_pred.type))
+        # check if y is of the correct datatype
+        if y.dtype.startswith('int'):
+            # the T.neq operator returns a vector of 0s and 1s, where 1
+            # represents a mistake in prediction
+            return T.mean(T.neq(self.y_pred, y))
+        else:
+            raise NotImplementedError()
         
-def generate_instances(corpus, vocab, context_sz):
+def make_instances(corpus, vocab, context_sz):
     def shared_dataset(data_xy, borrow=True):
         data_x, data_y = data_xy
         shared_x = theano.shared(np.asarray(data_x, dtype=np.int32), borrow=borrow)
@@ -98,7 +120,7 @@ def generate_instances(corpus, vocab, context_sz):
     data = []
     labels = []        
     for sentence in corpus:
-        # add 'start of sentence' / 'end of sentence' context
+        # add 'start of sentence' and 'end of sentence' context
         sentence = ['<s>'] * context_sz + sentence + ['</s>'] * context_sz
         sentence = vocab.doc_words_to_ids(sentence, update_dict=False, unk='<unk>')
         for instance in zip(*(sentence[i:] for i in xrange(context_sz+1))):
@@ -109,9 +131,12 @@ def generate_instances(corpus, vocab, context_sz):
     return train_set_x, train_set_y
 
 
-def train_lbl(train_data, dev_data = [], test_data = [], 
-              K = 20, context_sz = 2, learning_rate=0.1, 
-              epochs=10, batch_size=100, rng = None):
+def train_lbl(train_data, dev_data, test_data=[], 
+              K=20, context_sz=2, learning_rate=1.0, 
+              rate_update='simple', epochs=10, 
+              batch_size=100, rng=None, patience=None, 
+              patience_incr=2, improvement_thrs=0.995, 
+              validation_freq=1000):
     """
     Train log-bilinear model
     """
@@ -125,9 +150,9 @@ def train_lbl(train_data, dev_data = [], test_data = [],
     rng = np.random.RandomState() if not rng else rng
 
     # generate (context, target) pairs of word ids
-    train_set_x, train_set_y = generate_instances(train_data, vocab, context_sz)
-    dev_set_x, dev_set_y = generate_instances(dev_data, vocab, context_sz)
-    test_set_x, test_set_y = generate_instances(test_data, vocab, context_sz)
+    train_set_x, train_set_y = make_instances(train_data, vocab, context_sz)
+    dev_set_x, dev_set_y = make_instances(dev_data, vocab, context_sz)
+    test_set_x, test_set_y = make_instances(test_data, vocab, context_sz)
 
     # number of minibatches for training
     n_train_batches = train_set_x.get_value(borrow=True).shape[0] / batch_size
@@ -146,9 +171,6 @@ def train_lbl(train_data, dev_data = [], test_data = [],
     # cost function during training is negative log likelihood of the training data
     cost = lbl.negative_log_likelihood(y)
 
-    # perplexity of the data given the model
-    ppl = lbl.perplexity(y)
-
     # compute the gradient
     gparams = []
     for param in lbl.params:
@@ -158,72 +180,112 @@ def train_lbl(train_data, dev_data = [], test_data = [],
     # specify how to update the parameter of the model
     updates = []
     for param, gparam in zip(lbl.params, gparams):
-        updates.append((param, param - learning_rate * gparam))
+        updates.append((param, param-learning_rate*gparam))
 
-    # compiling theano functions that computes perplexity on the dev and test set
-    ppl_model_dev = theano.function(inputs=[index], outputs=ppl,
-                                 givens={x: dev_set_x[index * batch_size:(index+1) * batch_size],
-                                         y: dev_set_y[index * batch_size:(index+1) * batch_size]
+    # compiling theano functions that computes log-probability of the dev set
+    logprob_dev = theano.function(inputs=[index], outputs=cost,
+                                 givens={x: dev_set_x[index*batch_size:(index+1)*batch_size],
+                                         y: dev_set_y[index*batch_size:(index+1)*batch_size]
                                          })
 
-    ppl_model_test = theano.function(inputs=[index], outputs=ppl,
-                                 givens={x: test_set_x[index * batch_size:(index+1) * batch_size],
-                                         y: test_set_y[index * batch_size:(index+1) * batch_size]
+
+    # validate_model = theano.function(inputs=[index],
+    #                                  outputs=lbl.errors(y),
+    #                                  givens={ x: dev_set_x[index * batch_size:(index + 1) * batch_size],
+    #                                           y: dev_set_y[index * batch_size:(index + 1) * batch_size]})
+
+    # compiling theano functions that computes log-probability of the test set
+    logprob_test = theano.function(inputs=[index], outputs=cost,
+                                 givens={x: test_set_x[index*batch_size:(index+1)*batch_size],
+                                         y: test_set_y[index*batch_size:(index+1)*batch_size]
                                          })
     
     # compiling theano function 'train model' that returns the cost
     # and updates the parameter of the model 
     train_model = theano.function(inputs=[index], outputs=cost,
                                   updates=updates,
-                                  givens={x: train_set_x[index * batch_size:(index+1) * batch_size],
-                                          y: train_set_y[index * batch_size:(index+1) * batch_size]
+                                  givens={x: train_set_x[index*batch_size:(index+1)*batch_size],
+                                          y: train_set_y[index*batch_size:(index+1)*batch_size]
                                           })
 
-            
+    # perplexity functions
+    def compute_dev_logp():
+        return np.mean([logprob_dev(i) for i in xrange(n_dev_batches)])
+
+    def compute_dev_ppl():
+        return np.power(2.0, compute_dev_logp())
+
+    def compute_test_logp():
+        return np.mean([logprob_test(i) for i in xrange(n_test_batches)])
+
+    def compute_test_ppl():
+        return np.power(2.0, compute_test_logp())
+        
     # train model
     logger.info("training model...")
-    patience = 5000 
-    patience_increase = 2
-    improvement_threshold = 0.995
-    validation_freq = min(n_train_batches, patience/2)
     best_params = None
+    last_epoch_dev_ppl = np.inf
     best_dev_ppl = np.inf
     test_ppl = np.inf
     test_core = 0
     start_time = time.clock()
-
     done_looping = False
-    epoch = 0
-    while (epoch < epochs) and (not done_looping):
+
+    for epoch in xrange(epochs):
+        if done_looping:
+            break
         logger.debug('epoch %i' % epoch) 
         for minibatch_index in xrange(n_train_batches):
-            if minibatch_index > 0 and minibatch_index % 100 == 0:
-                logger.debug('minibatch %i' % minibatch_index) 
             minibatch_avg_cost  = train_model(minibatch_index)
             # iteration number
             itr = epoch * n_train_batches + minibatch_index            
             if itr % validation_freq == 0:
-                # compute perplexity on dev set
-                dev_ppl = np.mean([ppl_model_dev(i) for i in xrange(n_dev_batches)])
+                # compute perplexity on dev set, lower is better
+                dev_logp = compute_dev_logp()
+                dev_ppl = compute_dev_ppl()
                 logger.debug('epoch %i, minibatch %i/%i, dev ppl %.4f' % (epoch, minibatch_index+1,
                                                                                    n_train_batches, dev_ppl))
+                logger.debug('epoch %i, minibatch %i/%i, dev log prob %.4f' % (epoch, minibatch_index+1,
+                                                                                   n_train_batches, dev_logp))
+
+                validation_losses = [validate_model(i) for i in xrange(n_valid_batches)]
+                this_validation_loss = numpy.mean(validation_losses)
+                logger.debug('epoch %i, minibatch %i/%i, dev log prob %.4f' % (epoch, minibatch_index+1, this_validation_loss))
+
 
                 # if we got the lowest perplexity until now
                 if dev_ppl < best_dev_ppl:
                     # improve patience if loss improvement is good enough
-                    if dev_ppl < best_dev_ppl * improvement_threshold:
-                        patience = max(patience, itr * patience_increase)
+                    if patience and dev_ppl < best_dev_ppl * improvement_thrs:
+                        patience = max(patience, itr * patience_incr)
                     best_dev_ppl = dev_ppl
                     # test perplexity
-                    test_ppl = np.mean([ppl_model_test(i) for i in xrange(n_test_batches)])
+                    test_logp = compute_test_logp()
+                    test_ppl = compute_test_ppl()
                     logger.debug('  epoch %i, minibatch %i/%i, test ppl %.4f' % (epoch, minibatch_index+1, 
                                                                                   n_train_batches, test_ppl))
-                    
-            if patience <= itr:
+                    logger.debug('epoch %i, minibatch %i/%i, test log prob %.4f' % (epoch, minibatch_index+1,
+                                                                                    n_train_batches, test_logp))
+            # stop learning if no improvement was seen for a long time
+            if patience and patience <= itr:
                 done_looping = True
                 break
-        # increment epoch
-        epoch = epoch + 1
+        # adapt learning rate
+        if rate_update == 'simple':
+            # set learning rate to 1 / epoch index (1, 0.5, 0.33, ..)
+            learning_rate = 1.0 / (epoch+1)
+        elif rate_update == 'adaptive':
+            # half learning rate if perplexity increased at end of epoch
+            this_epoch_dev_ppl = compute_dev_ppl()
+            if this_epoch_dev_ppl > last_epoch_dev_ppl:
+                learning_rate /= 2.0
+            last_epoch_dev_ppl = this_epoch_dev_ppl
+        elif rate_update == 'constant':
+            # keep learning rate constant
+            pass
+        else:
+            raise ValueError("Unknown learning rate update strategy: %s" %rate_update)
+        
 
     end_time = time.clock()
     total_time = end_time - start_time
@@ -239,36 +301,42 @@ if __name__ == '__main__':
 
     # parse command line arguments
     arguments = docopt(__doc__)
-    log_level= logging.DEBUG if arguments['--verbose'] else logging.ERROR
+    log_level= logging.DEBUG if arguments['--verbose'] else logging.INFO
     FORMAT = '%(asctime)-15s %(message)s'
     logging.basicConfig(format=FORMAT)
     logger.setLevel(log_level)
-    word_dim = arguments['--word_dim'] or 20
-    context_sz = arguments['--context_sz'] or 2
-    learn_rate = arguments['--learn_rate'] or 0.1
-    epochs = arguments['--epochs'] or 10
-    batch_sz = arguments['--batch_size'] or 100
-    seed = arguments['--seed'] or None
+    word_dim = int(arguments['--word_dim'])
+    context_sz = int(arguments['--context_sz'])
+    learn_rate = float(arguments['--learn_rate'])
+    rate_update = arguments['--rate_update']
+    epochs = int(arguments['--epochs'])
+    batch_sz = int(arguments['--batch_size'])
+    seed = int(arguments['--seed']) if arguments['--seed'] else None
+    patience = int(arguments['--patience']) if arguments['--patience'] else None
+    patience_incr = int(arguments['--patience_incr'])
+    improvement_thrs = float(arguments['--improvement_thrs'])
+    validation_freq = int(arguments['--validation_freq'])
 
+    # load data
     logger.info("Load data ...")
-    with open(arguments['TRAIN_DATA'], 'rb') as fin:
-        train_data = [line.split() for line in fin.readlines() if line.strip()]
-    
-    if arguments['DEV_DATA']:
-        with open(arguments['DEV_DATA'], 'rb') as fin:
-            dev_data = [line.split() for line in fin.readlines() if line.strip()]
-    else:
-        dev_data = []
-
-    if arguments['TEST_DATA']:
-        with open(arguments['TEST_DATA'], 'rb') as fin:
+    with open(arguments['<train_data>'], 'rb') as fin:
+        train_data = [line.split() for line in fin.readlines() if line.strip()]    
+    with open(arguments['<dev_data>'], 'rb') as fin:
+        dev_data = [line.split() for line in fin.readlines() if line.strip()]
+    if arguments['<test_data>']:
+        with open(arguments['<test_data>'], 'rb') as fin:
             test_data = [line.split() for line in fin.readlines() if line.strip()]
     else:
         test_data = []
 
-    # random generator
+    # create random number generator
     rng_state = np.random.RandomState(seed)
 
     # train lm model
-    train_lbl(train_data, dev_data=dev_data, test_data = test_data, K=word_dim, context_sz=context_sz, learning_rate=learn_rate, epochs=epochs, batch_size = batch_sz, rng=rng_state)
+    train_lbl(train_data, dev_data, test_data=test_data, 
+              K=word_dim, context_sz=context_sz, learning_rate=learn_rate, 
+              rate_update=rate_update, epochs=epochs, batch_size = batch_sz, 
+              rng=rng_state, patience=patience, patience_incr=patience_incr, 
+              improvement_thrs=improvement_thrs, validation_freq=validation_freq)
+              
 
